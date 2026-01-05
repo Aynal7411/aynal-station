a85030a728de931d84c2b4ef875ee47c4ab4b524
aynalhaque <aynalhaque7411@gmail.com>
2026-01-05 20:13:13 +0600

    update

--- mern-portfolio/backend/server.js
+++ mern-portfolio/backend/server.js
@@ -16,7 +16,14 @@ const app = express();
 connectDB();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "https://aynal-station.netlify.app"
+  ],
+  credentials: true
+}));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
